4e347321d535999bc721630f48a9ef5295800f54
ifouzezi <ifoghaleuzezi@gmail.com>
2026-02-10 17:46:51 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./dist/**/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Figtree", "ui-sans-serif", "system-ui", "sans-serif"],
+      }
+    }
+  },
+  plugins: []
+}
